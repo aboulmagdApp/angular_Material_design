fd432fb912131a4a74a22c504d8010eb11aac373
Mohamed Aboulmagd <aboulmagd@live.com>
2019-10-27 10:36:34 +0300

    Adding a Tabs Component

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -9,7 +9,8 @@ import {
         MatCheckboxModule,
         MatSidenavModule,
         MatToolbarModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
 } from '@angular/material';
 @NgModule({
         imports: [MatButtonModule,
@@ -21,7 +22,8 @@ import {
                 MatCheckboxModule,
                 MatSidenavModule,
                 MatToolbarModule,
-                MatListModule
+                MatListModule,
+                MatTabsModule
         ],
         exports: [MatButtonModule,
                 MatIconModule,
@@ -32,7 +34,8 @@ import {
                 MatCheckboxModule,
                 MatSidenavModule,
                 MatToolbarModule,
-                MatListModule
+                MatListModule,
+                MatTabsModule
         ]
 })
 export class MaterialModule { }
