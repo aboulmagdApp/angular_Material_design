0a1c50e32630c25f6e9d7aebd9d4a495fb565b45
Mohamed Aboulmagd <aboulmagd@live.com>
2019-10-27 08:40:33 +0300

    intro angular material

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTraningComponent } from './training/current-traning/current-traning.component';
+import { NewTraningComponent } from './training/new-traning/new-traning.component';
+import { PastTraningComponent } from './training/past-traning/past-traning.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTraningComponent,
+    NewTraningComponent,
+    PastTraningComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
-
+import {
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule
+} from '@angular/material';
 @NgModule({
-    imports:[],
-    exports:[]
+        imports: [MatButtonModule,
+                MatIconModule,
+                MatFormFieldModule,
+                MatInputModule,
+                MatDatepickerModule,
+                MatNativeDateModule,
+                MatCheckboxModule,
+                MatSidenavModule,
+                MatToolbarModule,
+                MatListModule
+        ],
+        exports: [MatButtonModule,
+                MatIconModule,
+                MatFormFieldModule,
+                MatInputModule,
+                MatDatepickerModule,
+                MatNativeDateModule,
+                MatCheckboxModule,
+                MatSidenavModule,
+                MatToolbarModule,
+                MatListModule
+        ]
 })
-export class MaterialModule {}
+export class MaterialModule { }
